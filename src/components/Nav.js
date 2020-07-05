d06d87e21821db44a52efa27634043ef0dfc42a2
Daniel <danielprotr@gmail.com>
2020-07-05 14:56:23 -0400

    Search bar changes logo and menu items

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react'
 
 import './styles/Nav.css';
+import Logo from './Img/Connect.png'
 
 export class Nav extends Component {
+
     render() {
         return (
             <div className="Nav__container">
-                <div className="Nav__item Logo-container">
-                    <div>
-                        <img src="" alt="Connect logo"/>
-                        <p>Logo</p>
-                    </div>
+                <div className="Nav__item">
+                    <div className="Logo__container">
+                        <div className="Logo__item Logo__imagen">
+                            <img src={Logo} width="40" height="40" alt="Connect logo"/>
+                        </div>
+                        <div className="Logo__item Logo__texto">
+                            Logo
+                        </div>
+                    </div>                   
+                    
                 </div>                
                 
                 <div className="Nav__item Menu-container">
                     <div className="Menu__items">
-                        <p>Sign in</p>
+                        <p>Sign in <strong>|</strong></p>
                     </div>
                     <div className="Menu__items">
-                        <p>Sign up</p>
+                        <p>Sign up <strong>|</strong></p>
                     </div>
                     <div className="Menu__items">
                         <p>Menu</p>
