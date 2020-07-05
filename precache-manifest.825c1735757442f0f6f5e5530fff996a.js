4f7ada9332d1e20a2df205d3600db08ca7bafaf7
Daniel <danielprotr@gmail.com>
2020-07-05 04:48:41 -0400

    Updates

---
+++ precache-manifest.825c1735757442f0f6f5e5530fff996a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd81431d647dcd2d503d6e6bc2082239",
+    "url": "/Danielprotr/propstating.git/index.html"
+  },
+  {
+    "revision": "4ff4ec0082b6b5895541",
+    "url": "/Danielprotr/propstating.git/static/css/main.835090a4.chunk.css"
+  },
+  {
+    "revision": "3d52eb50cade8dd1c8c9",
+    "url": "/Danielprotr/propstating.git/static/js/2.d772c3dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Danielprotr/propstating.git/static/js/2.d772c3dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff4ec0082b6b5895541",
+    "url": "/Danielprotr/propstating.git/static/js/main.16eddd05.chunk.js"
+  },
+  {
+    "revision": "68d40fffcc9f240dac86",
+    "url": "/Danielprotr/propstating.git/static/js/runtime-main.0ee64490.js"
+  }
+]);
