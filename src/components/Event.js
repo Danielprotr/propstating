de782079313413f51def56f67d2208ea40915c4b
Daniel <danielprotr@gmail.com>
2020-07-08 08:31:11 -0400

    status event tests

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,15 +4,21 @@ import './styles/Event.css'
 
 export class Event extends Component {
 
+    state = {};
+
     handleChange = e =>{
-        console.log({
+        /* console.log({
             name: e.target.name,
             value: e.target.value,
+        }); */
+
+        this.setState({
+            [e.target.name]: e.target.value
         });
     }  
 
     handleClick = e =>{
-        console.log("me diste click!")
+        console.log("me diste click!")        
     }
 
     handleSubmit = e => {
@@ -35,7 +41,21 @@ export class Event extends Component {
                         className="test_input" 
                         onChange={this.handleChange}
                         type="text" 
-                        name="textoInputiado"/>
+                        name="input 1"
+                        value={this.state.input1}
+                    />
+                    <br/>
+                    <input 
+                        className="test_input" 
+                        onChange={this.handleChange}
+                        type="text" 
+                        name="input 2"/>
+                    <br/>
+                        <input 
+                        className="test_input" 
+                        onChange={this.handleChange}
+                        type="text" 
+                        name="input 3"/>
                     <button 
                         className="test_btn"
                         /* type="button" */
